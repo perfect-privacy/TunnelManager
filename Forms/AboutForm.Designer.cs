641c31f10efbfc3e771e468614d82f87ac2377d4
Lars <admin@perfect-privacy.com>
2013-10-01 15:12:29 +0000

    version number update

--- Forms/AboutForm.Designer.cs
+++ Forms/AboutForm.Designer.cs
@@ -52,7 +52,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(66, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Version 1.5";
+            this.label2.Text = "Version 1.5"; 
             // 
             // label1
             // 
